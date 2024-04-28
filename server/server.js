5131cdcf115625fb51c8e6ff0c0dbd9b36aaa61d
Woosung_Kim <sibu2005@naver.com>
2024-04-28 14:30:35 +0900

    feat: 간단하게 이미지를 받아서 서버에 저장하는 로직 구현

--- server/server.js
+++ server/server.js
@@ -1,6 +1,39 @@
 const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+const { v4: uuid } = require('uuid');
+const mime = require('mime-types');
+
+
+const dir = './public';
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, dir),
+    filename: (req, file, cb) => cb(null, `${uuid()}.${mime.extension(file.mimetype)}`)
+  })
+
+const upload = multer({ storage: storage, fileFilter: (req, file, cb) => {
+    if ([ 'image/png', 'image/jpeg', 'image/jpg'].includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new Error('Only image files are allowed!'), false);
+    }},
+    limits: {fileSize: 1024 * 1024 * 5}  // 10MB
+});
+
+if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+
 
 const app = express();
+
+//app.use(express.static('public'));  // root 주소
+
+app.use("/public", express.static('public'));  // /public 주소
+
+
+app.post('/upload', upload.single("imageTest"), (req, res) => {
+    console.log(req.file)
+    res.json(req.file)
+})
 
 
 app.listen(8080, () => console.info("Server is running on port 8080"));
